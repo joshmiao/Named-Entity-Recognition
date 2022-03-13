9e49be72953ba72b6f0580ab8ac0f2b5cafdca7f
joshmiao <myk0210@163.com>
2022-03-13 17:29:49 +0800

    update

--- main.py
+++ main.py
@@ -8,16 +8,51 @@ the 'data' array contains 'word' in the following format:
     organization_mark : {0, 1, 2} 
     place_mark : {0, 1, 2}
     name_mark : {0, 1, 2}
-    0 for B(Begin), 1 for I(Inside), 2 for O(Outside)
+    0 for O(Outside), 1 for B(Begin), 2 for I(Inside)
 '''
 data = []
+
+# processing and marking data
 
 for lines in data_source:
     line_data = lines.split('  ')
     for word in line_data:
-        data.append(word.split('/'))
+        if len(word.split('/')) == 2:
+            data.append(word.split('/'))
 
 for word in data:
-    mark = [0, 0, 0]
+    mark = []
+    mark.append(1) if word[1] == 'nt' else mark.append(0)
+    mark.append(1) if word[1] == 'ns' else mark.append(0)
+    mark.append(1) if word[1] == 'nr' else mark.append(0)
+    word += mark
 
-
+# print(data)
+idx = 0
+while idx < len(data):
+    if data[idx][0].find('[') != -1:
+        end_id = idx + 1
+        while data[end_id][1].find(']') == -1:
+            # print(data[end_id])
+            end_id += 1
+        ty = data[end_id][1].split(']')[1]
+        data[end_id][1] = data[end_id][1].split(']')[0]
+        data[idx][0] = data[idx][0].split('[')[1]
+        if ty == 'nt':
+            data[idx][2] = 1
+        elif ty == 'ns':
+            data[idx][3] = 1
+        elif ty == 'nr':
+            data[idx][4] = 1
+        idx += 1
+        while idx <= end_id:
+            if ty == 'nt':
+                data[idx][2] = 2
+            elif ty == 'ns':
+                data[idx][3] = 2
+            elif ty == 'nr':
+                data[idx][4] = 2
+            idx += 1
+    else:
+        idx += 1
+print(data)
