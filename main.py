c2795338a7976f55ed37aefe24aab3a0ee044c96
joshmiao <myk0210@163.com>
2022-03-13 16:42:41 +0800

    update

--- main.py
+++ main.py
@@ -1,82 +1,23 @@
-import torch
-import math
+data_source = open("./data_source/data_source.txt", "r")
 
-class LegendrePolynomial3(torch.autograd.Function):
-    """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
-    """
+'''
+the 'data' array contains 'word' in the following format:
+    word : [text, type, organization_mark, place_mark, name_mark]
+    text : {string}
+    type : {string}
+    organization_mark : {0, 1, 2} 
+    place_mark : {0, 1, 2}
+    name_mark : {0, 1, 2}
+    0 for B(Begin), 1 for I(Inside), 2 for O(Outside)
+'''
+data = []
 
-    @staticmethod
-    def forward(ctx, input):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation. You can cache arbitrary
-        objects for use in the backward pass using the ctx.save_for_backward method.
-        """
-        ctx.save_for_backward(input)
-        return 0.5 * (5 * input ** 3 - 3 * input)
+for lines in data_source:
+    line_data = lines.split('  ')
+    for word in line_data:
+        data.append(word.split('/'))
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-        """
-        input, = ctx.saved_tensors
-        return grad_output * 1.5 * (5 * input ** 2 - 1)
+for word in data:
+    mark = [0, 0, 0]
 
-dtype = torch.float
-device = torch.device("cpu")
-# device = torch.device("cuda:0")  # Uncomment this to run on GPU
 
-# Create Tensors to hold input and outputs.
-# By default, requires_grad=False, which indicates that we do not need to
-# compute gradients with respect to these Tensors during the backward pass.
-x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
-y = torch.sin(x)
-
-# Create random Tensors for weights. For this example, we need
-# 4 weights: y = a + b * P3(c + d * x), these weights need to be initialized
-# not too far from the correct result to ensure convergence.
-# Setting requires_grad=True indicates that we want to compute gradients with
-# respect to these Tensors during the backward pass.
-a = torch.full((), 0.0, device=device, dtype=dtype, requires_grad=True)
-b = torch.full((), -1.0, device=device, dtype=dtype, requires_grad=True)
-c = torch.full((), 0.0, device=device, dtype=dtype, requires_grad=True)
-d = torch.full((), 0.3, device=device, dtype=dtype, requires_grad=True)
-
-learning_rate = 5e-6
-for t in range(200000):
-    # To apply our Function, we use Function.apply method. We alias this as 'P3'.
-    P3 = LegendrePolynomial3.apply
-
-    # Forward pass: compute predicted y using operations; we compute
-    # P3 using our custom autograd operation.
-    y_pred = a + b * P3(c + d * x)
-
-    # Compute and print loss
-    loss = (y_pred - y).pow(2).sum()
-    if t % 10000 == 99:
-        print(t, loss.item())
-
-    # Use autograd to compute the backward pass.
-    loss.backward()
-
-    # Update weights using gradient descent
-    with torch.no_grad():
-        a -= learning_rate * a.grad
-        b -= learning_rate * b.grad
-        c -= learning_rate * c.grad
-        d -= learning_rate * d.grad
-
-        # Manually zero the gradients after updating weights
-        a.grad = None
-        b.grad = None
-        c.grad = None
-        d.grad = None
-
-print(f'Result: y = {a.item()} + {b.item()} * P3({c.item()} + {d.item()} x)')
